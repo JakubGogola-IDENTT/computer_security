4dc370c8ac1384e918b4c8d0236e0131a7774b95
JakubGogola <236412@student.pwr.edu.pl>
2018-10-27 21:43:41 +0200

    Cosmetic changes

--- lista3/session.py
+++ lista3/session.py
@@ -23,10 +23,10 @@ def get_cookie():
                 pattern['name'] = k
                 pattern['value'] = v
             print(pattern)
-            driver = webdriver.Chrome()
+            driver = webdriver.Firefox()
             driver.get('http://mikolaj.ovh')
             driver.add_cookie(pattern)
             driver.get('http://mikolaj.ovh')
-
+            
 
 get_cookie()
