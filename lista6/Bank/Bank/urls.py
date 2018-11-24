632d2a03f897ef5b683e12538310fa6c714e4745
Jakub Gogola <jakubgogola97@gmail.com>
2018-11-24 18:59:50 +0100

    registration implemented

--- lista6/Bank/Bank/urls.py
+++ lista6/Bank/Bank/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('')
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
