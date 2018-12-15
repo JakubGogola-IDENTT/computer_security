67a5a7628de6c7faebc71db89ab5e6ba0201e837
Jakub Gogola <jakubgogola97@gmail.com>
2018-12-15 16:56:34 +0100

    removed

--- lista8/Bank/AwesomeBank/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from AwesomeBank.models import PreparedTransfer
-from AwesomeBank.models import Transfer
-
-# Register your models here.
-admin.site.register(PreparedTransfer)
-admin.site.register(Transfer)
--- lista8/Bank/accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.urls import reverse_lazy
-from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
-
-
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
